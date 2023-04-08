174adf6f90db23fb54638c358e8b048242a8e164
Zachary <zachary@Zipcoders-MBP-2.fios-router.home>
2023-04-08 19:02:27 -0400

    file is building

--- Client/src/main/java/controllers/ServerController.java
+++ Client/src/main/java/controllers/ServerController.java
@@ -1,13 +1,23 @@
-import spiffyUrlManipulator
+package controllers;
+//import spiffyUrlManipulator;
+import models.Id;
 
-public class ServerController() {
-    private String rootURL = "http://zipcode.rocks:8085";
+import javax.json.JsonString;
+import javax.swing.*;
+import java.net.HttpURLConnection;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+
+public class ServerController {
+    private String rootURL = "http://zipcode.rocks:8085/ids";
 
     private ServerController svr = new ServerController();
 
     private ServerController() {}
 
-    public static shared() {
+    public ServerController shared() {
         return svr;
     }
 
@@ -15,16 +25,24 @@ public class ServerController() {
         // url -> /ids/
         // send the server a get with url
         // return json from server
+//                {
+//                    "userid": " ",
+//                "name": "defect",
+//                "github": "defect-account"
+//},
+        return null;
     }
-    public JsonString idPost(Id) {
+    public JsonString idPost(Id id) {
         // url -> /ids/
         // create json from Id
         // request
         // reply
         // return json
+        return null;
     }
-    public JsonString idPut(Id) {
+    public JsonString idPut(Id id) {
         // url -> /ids/
+        return null;
     }
 
 
